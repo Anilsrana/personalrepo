c0b0dc17478f30eb6101030ecaf9ccc96860d91c
Anil S personal laptop <anilsrana7@gmail.com>
2024-12-28 20:12:13 +0530

    add country code

--- docker-project/src/app.py
+++ docker-project/src/app.py
@@ -8,7 +8,10 @@ def index():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
+        response = requests.get('https://ipinfo.io')
+        data = response.json()
+        country_code = data.get('country', 'Unknown') 
+        return render_template('index.html', hostname=host_name, ip=host_ip, country_code=country_code )
     except:
         return render_template('error.html')
 
